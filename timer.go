6943fe6d87877cd49c0e4c9ad42cd3e6d1d8198f
lyderic <lyderic@nuc.lyderic.net>
2018-06-20 12:22:30 +0100

    0.0.2: better prototype timer, struggling with buffered inputs!

--- timer.go
+++ timer.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"time"
 )
 
 func timer() (err error) {
-	fmt.Println("Prototype Timer, hit Ctrl-C to stop")
-	hideCursor()
+	fmt.Println("Prototype Timer, Ctrl-C to stop")
+	exec.Command("stty", "-F", "/dev/tty", "-icanon", "-echo", "min", "1", "time", "10").Run()
+	defer exec.Command("stty", "-F", "/dev/tty", "sane").Run()
 	start := time.Now()
 	for {
-		fmt.Print(time.Now().Sub(start))
 		time.Sleep(time.Duration(500000) * time.Nanosecond)
-		wipeLine()
+		fmt.Printf("\r%s\r", time.Now().Sub(start))
+		var b []byte
+		os.Stdin.Read(b)
+		if len(b) == 0 { continue }
+		if b[0] == 32 {
+			break
+		}
 	}
 	fmt.Println()
 	fmt.Println("Your time is:", time.Now().Sub(start))
-	showCursor()
 	return
 }
