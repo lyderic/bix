a2cf483a9330b03c1dedad925fc529e7cad0879c
lyderic <lyderic@nuc.lyderic.net>
2018-06-22 10:04:08 +0100

    0.0.6: prevent Ctrl-C to mess up terminal when timer loops

--- constants.go
+++ constants.go
@@ -1,5 +1,5 @@
 package main
 
 const (
-	VERSION = "0.0.5"
+	VERSION = "0.0.6"
 )
--- timer.go
+++ timer.go
@@ -3,17 +3,39 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
 	"time"
 )
 
 func timer() (err error) {
-	fmt.Println("Prototype Timer, hit [SPACE] to stop")
-	stty("-icanon", "-echo", "min", "0", "time", "0")
-	defer stty("sane")
+	fmt.Println("Timer - hit [SPACE] to stop")
+	if err = setTerminal(); err != nil {
+		return
+	}
+	start := time.Now()
+	// the next two lines to prevent Ctrl-C to be pressed as it messes up the
+	// terminal
+	c := make(chan os.Signal)
+	//signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(c, os.Interrupt)
+	loop()
+	fmt.Println("Your time is:", time.Now().Sub(start))
+	return restoreTerminal()
+}
+
+func setTerminal() (err error) {
+	hideCursor()
+	return stty("-icanon", "-echo", "min", "0", "time", "0")
+}
+
+func restoreTerminal() (err error) {
+	showCursor()
+	return stty("sane")
+}
+
+func loop() {
 	start := time.Now()
 	var b []byte = make([]byte, 1)
-	hideCursor()
-	defer showCursor()
 	for {
 		time.Sleep(time.Duration(500000) * time.Nanosecond)
 		fmt.Printf("\r%s\r", time.Now().Sub(start))
@@ -25,6 +47,4 @@ func timer() (err error) {
 			break
 		}
 	}
-	fmt.Println("Your time is:", time.Now().Sub(start))
-	return
 }
